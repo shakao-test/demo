990e59bf25a62fffa0a3319645c1c700a28480c5
shakao-test <61802990+shakao-test@users.noreply.github.com>
2021-07-21 14:06:23 -0700

    0.0.55

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.4.45/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.54","target":"arcade","targetVersion":"1.4.45","repo":"shakao-test/demo"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.4.45/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.55","target":"arcade","targetVersion":"1.4.45","repo":"shakao-test/demo"}
 // total=1130596 new=6.24% cached=88.12% other=5.64%
 (function (ectx) {
 'use strict';
@@ -383,7 +383,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P16782(s) {
+function _main___P17034(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -751,13 +751,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P16782.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"pxt_modules/device/startup.ts","functionName":"<main>","argumentNames":[]}
-_main___P16782.continuations = [  ]
+_main___P17034.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"pxt_modules/device/startup.ts","functionName":"<main>","argumentNames":[]}
+_main___P17034.continuations = [  ]
 
-function _main___P16782_mk(s) {
+function _main___P17034_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P16782, depth: s.depth + 1,
+        parent: s, fn: _main___P17034, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -38778,5 +38778,5 @@ const sprites_StaticObstacle__C2168_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P16782
+return _main___P17034
 })
